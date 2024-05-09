c2ba9498892991f7687765a1731568451660c7f9
amoofygithub <hello@amoofy.com>
2024-05-09 05:10:16 +0000

    Adding a second tab to the app.

--- pages/Amoofy_Demo.py
+++ pages/Amoofy_Demo.py
@@ -14,10 +14,12 @@
 
 import streamlit as st
 from openai import OpenAI
+import os
 import pandas as pd
 from datetime import datetime
-import numpy as np
-import os
+from wordcloud import WordCloud
+import random
+import matplotlib.pyplot as plt
 
 def amoofy_demo():
 
@@ -25,6 +27,16 @@ def amoofy_demo():
     client = OpenAI(
     api_key = st.secrets.api.key
     )
+
+    #filtering words
+    def filter_short_words(text):
+        return " ".join([str(word).lower() for word in text.split() if len(word) >= 5])
+
+    # Custom color function for the word cloud
+    def custom_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
+        colors = ["#b19cd9", "#dcd0ff", "#ffb6c1", "#bebebe"]
+        return random.choice(colors)
+
 
     # Upload and display logo
     logo_path = "./pages/files/amoofy.png"
@@ -35,27 +47,47 @@ def amoofy_demo():
     # Streamlit App
     st.subheader(":studio_microphone: Interview Catalyst :studio_microphone: ")
     st.caption("Thank you for engaging with this demo. Please note that your information may be recorded for learning purposes.")
+    st.caption("In this demo, you’ll provide information for a one-time conversation with each person you wish to connect to.")
     st.divider()
 
-    # if st.button(":microphone: My First Interview :microphone:"):
-    with st.form("my_form"):
-        interviewer_name = st.text_input("Your First Name")
-        guest_name = st.text_input("Guest First Name")
-        relationship = st.text_input("What is your relationship to your guest?")
-        st.caption("Feel free to elaborate! The more context, the better!")
-        how_know_each_other = st.text_area("How do you know each other?")
-        st.caption("Tell us a story of how you met, where you met, or who introduced you.")
-        current_happenings = st.text_area("What is currently happening in your guest's life?")
-        current_happenings_you = st.text_area("What is currently happening in your life?")
-        st.caption("Sharing about what is going on in either of your lives can make for richer conversation. Please provide detail.")
-        submitted = st.form_submit_button("Get Questions!")
+    if 'extra_context' not in st.session_state:
+        st.session_state['extra_context'] = ""
 
-    log_df = pd.DataFrame(columns=["Interviewer", "Guest", "Relationship", "Context", "Current Happenings", "Current Happenings Me", "Date", "Suggested Questions"])
+    tab1, tab2 = st.tabs(["My First Interview :studio_microphone:", "Keeping the Conversation Going :speech_balloon:"])
 
-    if submitted:
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Get current date and time
-        combined_context = f"You are a nice interview assistant trying to help {interviewer_name} and {guest_name} reconnect. \
+    with tab1:
+        # if st.button(":microphone: My First Interview :microphone:"):
+        with st.form("my_form"):
+            interviewer_name = st.text_input("Your First Name")
+            guest_name = st.text_input("Your Guest's First Name")
+            st.caption("Who would you like to get to know better?")
+            st.write("\n\n")
+            relationship = st.text_input("What is your relationship with this person?")
+            st.caption("Tell us a story of how you met, where you met, or who introduced you,\
+                        or if this person is within your family, elaborate who this person is to you. \
+                        Feel free to elaborate! The more context, the better!")
+            st.write("\n\n")
+            how_know_each_other = st.text_area("How do you know each other?")
+            st.caption("Tell us a story of how you met, where you met, or who introduced you.")
+            st.write("\n\n")
+            importance = st.text_area("Why is it important for you to capture their story?")
+            st.caption("Can you tell us what is your reason to ask this person something, \
+                    what do you wish to know about them or any specific moments or advice you wish to ask for.")
+            st.write("\n\n")
+            current_happenings = st.text_area("Can you give a brief background about the person you want to interview?")
+            st.caption("Can you provide any context on what is happening in their life: where do they live, work, etc?")
+            st.write("\n\n")
+            current_happenings_you = st.text_area("Can you give a brief background about yourself?")
+            st.caption("Provide some context on what is happening in your life: where you live, work, etc.")
+            submitted = st.form_submit_button("Get Questions!")
+
+        # log_df = pd.DataFrame(columns=["Interviewer", "Guest", "Relationship", "Context", "Current Happenings", "Current Happenings Me", "Date", "Suggested Questions"])
+
+        if submitted:
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Get current date and time
+            combined_context = f"You are a nice interview assistant trying to help {interviewer_name} and {guest_name} reconnect. \
             Their relationship can be described this way: {relationship}.\
+            This is why it is important for {interviewer_name} to connection with {guest_name}: {importance}.\
             This is how they met: {how_know_each_other}.\
             This is relevant information about what the guest is doing currently in their life: {current_happenings}.\
             This is relevant information about what the interviewer is doing currently in their life: {current_happenings_you}.\
@@ -64,37 +96,105 @@ def amoofy_demo():
             You must give {interviewer_name} 3 questions that use information about {guest_name} to start the conversation. \
             If there is a connection between {current_happenings} and {current_happenings_you} you can refer to that in your question.\
             You MUST think of how to make your questions interesting, in the style of someone that is an expert interviewer like Oprah."
+
+            st.session_state['extra_context'] = combined_context 
+            # Display the updated DataFrame and text
+            st.divider()
+
+            # Generate follow-up questions
+            system_prompt = """You are a nice interview assistant.
+                You are STRICTLY FORBIDDEN from generating any questions that are offensive, provocative, or disrespectful.
+                            """
+
+            completion = client.chat.completions.create(
+                model="gpt-4",
+                messages=[{"role":"system", "content": f"{system_prompt}"},
+                    {"role": "user", "content": f"{combined_context}",}
+                    ]
+                    )	
+            st.write("Here you go:")
+            st.write(completion.choices[0].message.content)
+
+            # new_row = {
+            #     "Interviewer": interviewer_name,
+            #     "Guest": guest_name,
+            #     "Relationship": relationship,
+            #     "Context": how_know_each_other,
+            #     "Current Happenings": current_happenings,
+            #     "Current Happenings Me": current_happenings_you,
+            #     "Date": current_time,
+            #     "Suggested Questions": completion.choices[0].message.content
+            # }
+            # Append data to the DataFrame
+            # log_df = pd.concat([log_df, pd.DataFrame([new_row])], ignore_index=True)
+
+            # log_df.to_csv("./pages/files/interview_log.csv", index=False,mode='a', header=False)
+
+            st.divider()
+
+    with tab2:
+        st.subheader("Keep the Conversation Going...",divider='rainbow')
+
+        # if st.session_state['extra_context']:
+        #     st.write(f"The extra context entered in Tab 1 is: {st.session_state['extra_context']}")
+        # else:
+        #     st.write("No extra_context has been entered in Tab 1 yet.")
+
+
+        with st.form("my_form_2"):     
+            q_and_a = st.text_area("Select whichever questions you wish from the choices provided. \
+                                Paste into the following window, and add your guest's answers.\
+                                Make sure to define who is speaking for each question or answer.")
+            submitted = st.form_submit_button("Keep the Conversation Going")
+
+        if submitted:
+
+            st.subheader(":cloud: Your Conversational Word Cloud :cloud:", divider= 'rainbow')
+            
         
-        st.divider()
+            wordcloud = WordCloud(stopwords={f"{guest_name}",f"{interviewer_name}"}, color_func=custom_color_func, width=800, background_color="white", height=400).generate(filter_short_words(q_and_a))
+            fig, ax = plt.subplots(figsize=(10, 5))
+            ax.imshow(wordcloud, interpolation="bilinear")
+            ax.axis("off")
+            st.pyplot(fig)
 
-        # Generate follow-up questions
-        system_prompt = """You are a nice interview assistant.
-            You are STRICTLY FORBIDDEN from generating any questions that are offensive, provocative, or disrespectful.
-                        """
+            st.subheader(":speaking_head_in_silhouette: Your Conversational Summary and Next Steps :speaking_head_in_silhouette:", divider= 'rainbow')
+            
+            
+            new_context = f"{interviewer_name} and {guest_name} have had their first conversation using the questions you suggested previously.\
+                You already know this about them:\
+                Their relationship can be described this way: {relationship}.\
+                This is why it is important for {interviewer_name} to connection with {guest_name}: {importance}.\
+                This is how they met: {how_know_each_other}.\
+                This is relevant information about what the guest is doing currently in their life: {current_happenings}.\
+                This is relevant information about what the interviewer is doing currently in their life: {current_happenings_you}.\
+                \n \
+                The full conversation they had after they used your suggested questions is found here: {q_and_a}.\
+                You MUST help {interviewer_name} create 3 additional questions that they can use to keep the conversation going \
+                with {guest_name}. You MUST think of how to make your questions interesting, in the style of someone that is an expert interviewer like Oprah.\
+                You are STRICTLY FORBIDDEN from generating any questions that are offensive, provocative, or disrespectful.\
+                "
 
-        completion = client.chat.completions.create(
-            model="gpt-4",
-            messages=[{"role":"system", "content": f"{system_prompt}"},
-                {"role": "user", "content": f"{combined_context}",}
-                ]
-                )	
-        st.write("Here you go:")
-        st.write(completion.choices[0].message.content)
+            # Display the updated DataFrame and text
+            st.divider()
 
-        new_row = {
-            "Interviewer": interviewer_name,
-            "Guest": guest_name,
-            "Relationship": relationship,
-            "Context": how_know_each_other,
-            "Current Happenings": current_happenings,
-            "Current Happenings Me": current_happenings_you,
-            "Date": current_time,
-            "Suggested Questions": completion.choices[0].message.content
-        }
-        # Append data to the DataFrame
-        log_df = pd.concat([log_df, pd.DataFrame([new_row])], ignore_index=True)
 
-        log_df.to_csv("./pages/files/interview_log.csv", index=False,mode='a', header=False)
+            # Generate follow-up questions
+            system_prompt_2 = """You are a nice interview assistant.
+                You are STRICTLY FORBIDDEN from generating any questions that are offensive, provocative, or disrespectful.
+                Give me an analysis of the conversation thus far in the following format:
+                First, give a brief summary of what they have covered so far with the questions and answers.
+                Second, give 3 follow up questions that can help these two people continue the conversation in the future.
+                            """
+
+            completion_2 = client.chat.completions.create(
+                model="gpt-4",
+                messages=[{"role":"system", "content": f"{system_prompt_2}"},
+                    {"role": "user", "content": f"{new_context}",}
+                    ]
+                    )	
+            st.write("Summary and Follow Up Questions:")
+            st.write(completion_2.choices[0].message.content)
 
 st.set_page_config(page_title="Amoofy Demo", page_icon="🎙️")
 st.markdown("# Amoofy Demo")
